20059dd12151fe5c09c8a9bc61db92d56cb556e5
rushi444 <rushi44@gmail.com>
2020-09-20 21:05:44 -0700

    connect to discord

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-extern crate serenity;
-
 use serenity::prelude::*;
 use serenity::model::gateway::Ready;
 
-const TOKEN:: &str = "";
+const TOKEN: &str = "NzU3NDQ2Nzc3MTk1NzI0OTMx.X2ghSA.yVIMJr3EzsZLq6IZAiAwWETwsDo";
 
 struct Handler;
 
@@ -11,7 +9,7 @@ impl EventHandler for Handler {}
 
 fn main(){
     let mut client = Client::new(&TOKEN, Handler)
-                            .expect("Error creating client");
+                        .expect("Error creating client");
 
     if let Err(msg) = client.start() {
         println!("Error {:?}", msg)
