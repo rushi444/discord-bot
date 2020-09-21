b2116f3df28001b5fcaa94be014431308530b33b
rushi444 <rushi44@gmail.com>
2020-09-20 21:17:37 -0700

    test command ping => pong

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
-use serenity::prelude::*;
+use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
+use serenity::prelude::*;
 
 const TOKEN: &str = "NzU3NDQ2Nzc3MTk1NzI0OTMx.X2ghSA.yVIMJr3EzsZLq6IZAiAwWETwsDo";
 
 struct Handler;
 
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "?ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong?") {
+                println!("Error giving message: {:?}", why)
+            }
+        }
+    }
 
-fn main(){
-    let mut client = Client::new(&TOKEN, Handler)
-                        .expect("Error creating client");
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is ready", ready.user.name);
+    }
+}
+
+fn main() {
+    let mut client = Client::new(&TOKEN, Handler).expect("Error creating client");
 
     if let Err(msg) = client.start() {
         println!("Error {:?}", msg)
