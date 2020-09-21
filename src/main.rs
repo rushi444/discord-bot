ef15f8ff7688900559af4e1db16ff5cc022ba003
rushi444 <rushi44@gmail.com>
2020-09-20 20:37:59 -0700

    initialize client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+extern crate serenity;
+
+use serenity::prelude::*;
+use serenity::model::gateway::Ready;
+
+const TOKEN:: &str = "";
+
+struct Handler;
+
+impl EventHandler for Handler {}
+
+fn main(){
+    let mut client = Client::new(&TOKEN, Handler)
+                            .expect("Error creating client");
+
+    if let Err(msg) = client.start() {
+        println!("Error {:?}", msg)
+    }
 }
