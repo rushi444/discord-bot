ae539e94faf2f41ede81125b5386933ee971ecee
Rushi Arumalla <rushiarumalla@northwesternmutual.com>
2020-09-21 19:58:28 -0700

    reaction to reaction

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,39 @@
 use serenity::model::channel::Message;
+use serenity::model::channel::Reaction;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 
 struct Handler;
 
 impl EventHandler for Handler {
-    fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "?ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong?") {
-                println!("Error giving message: {:?}", why)
-            }
-        }
-    }
+	fn reaction_add(&self, ctx: Context, reaction: Reaction) {
+		if let Err(why) = reaction
+			.channel_id
+			.say(&ctx.http, format!("{} left a reaction", reaction.user_id))
+		{
+			println!("Error reacting to a reaction: {:?}", why)
+		}
+	}
 
-    fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is ready", ready.user.name);
-    }
+	fn message(&self, ctx: Context, msg: Message) {
+		if msg.content == "?ping" {
+			if let Err(why) = msg.channel_id.say(&ctx.http, "Pong?") {
+				println!("Error giving message: {:?}", why)
+			}
+		}
+	}
+
+	fn ready(&self, _: Context, ready: Ready) {
+		println!("{} is ready", ready.user.name);
+	}
 }
 
 fn main() {
-
 	let token = include_str!("../token");
 
-    let mut client = Client::new(&token, Handler).expect("Error creating client");
+	let mut client = Client::new(&token, Handler).expect("Error creating client");
 
-    if let Err(msg) = client.start() {
-        println!("Error {:?}", msg)
-    }
+	if let Err(msg) = client.start() {
+		println!("Error {:?}", msg)
+	}
 }
